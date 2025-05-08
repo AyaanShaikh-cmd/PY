966f1e9b1b6d506d71afa701e9f62dab9de8206f
Ayaan Shaikh <ayaanzshaikh2009@gmail.com>
2025-05-08 15:52:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-for i in range(1, n + 1):
-    print(' ' * (n - i), end='')
-    print('*' * (2 * i - 1))
-height = 6
+def print_pyramid(n):
+    for i in range(1, n + 1):
+        print(' ' * (n - i), end='')
+        print('*' * (2 * i - 1))
+
+
+height = 5
+print_pyramid(height)
